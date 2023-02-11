c52bfc4a501ad5951c2c5fff6fcbecfc7de0477c
divya-soni-14 <ds952073@student.nitw.ac.in>
2023-02-11 09:00:21 +0530

    react project initialized

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import "./css/Navbar.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
